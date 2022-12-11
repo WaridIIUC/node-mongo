deaf7de188f7a23b9ac12baaef3d16ae0a297b99
WaridIIUC <76819855+WaridIIUC@users.noreply.github.com>
2022-12-11 21:21:48 +0600

    update

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
-
+const cors = require('cors');
 
 
 const app = express();
 
+app.use(cors());
+
+const name = [{name: "karim"}, {name: "karim mia"}];
 app.get('/', function (req, res) {
-    res.send('hello world')
+    // res.send('hello world Nodemon')
+
+    const fruit = {
+        product: 'ada',
+        price: 50
+    }
+
+    res.send(fruit);
   })
+
+
+  app.get('/users/:id', function (req, res) {
+    const id = req.params.id;
+    const nameU = name[id];
+    res.send(nameU);
+  })
+
 
 
 app.listen(5000, () => console.log('Listening to port 5000'));
